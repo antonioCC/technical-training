4651a88e812b1d80f3c740bbcaf1a50f22b1691b
antonio contreras <you@amtonioalberto123.aacc@gmail.com>
2024-09-09 21:03:25 +0000

    primero

---
+++ estate/models/projecto.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+
+class TestModel(models.Model):
+    _name = "test.model"
+    _description = "test model"
+    
+    
+
+    name = fields.Char(required=True)
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(selection=[('North'), ('South'), ('East'), ('West')])
