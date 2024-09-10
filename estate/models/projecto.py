161f561bdc5d11632e88e6e4199ae0bdf6f0dded
antonio contreras <you@amtonioalberto123.aacc@gmail.com>
2024-09-10 16:30:08 +0000

    cap 5 listo

--- estate/models/projecto.py
+++ estate/models/projecto.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class TestModel(models.Model):
@@ -8,14 +9,19 @@ class TestModel(models.Model):
     
 
     name = fields.Char(required=True)
+    description = fields.Char()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    date_availability = fields.Date(copy=False,default=fields.Date.today()+ relativedelta(months=3))
+    expected_price = fields.Float(required=True,readonly=True)
+    selling_price = fields.Float(copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(selection=[('north','North'), ('south','South'), ('east','East'), ('west','West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(required=True,copy=False,default='nuevo',selection=[('nuevo','Nuevo'),('oferta recibida','Oferta recibida'),('oferta aceptada','Oferta aceptada'),('vendido','Vendido') ,('cancelado','Cancelado')])
+    
+    
